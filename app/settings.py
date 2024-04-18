8d148e9b6b6f2741531bedf42e322f4fa83135c7
JustVov4uk <voovvaaa@gmail.com>
2024-04-18 19:18:32 +0200

    postgre

--- app/settings.py
+++ app/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'deluxe',
+        'USER': 'deluxe',
+        'PASSWORD': 'deluxe',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
